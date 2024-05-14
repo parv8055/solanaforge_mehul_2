d9771a594819105145c424911bdb3a16245d2cfd
parv8055 <choudharypar4@gmail.com>
2024-05-14 12:35:45 +0530

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+
+export function Sidebar() {
+  return (
+    <div className="w-2/12 p-4 border-r-2 border-gray-100 space-y-8 hidden md:block sticky top-16 left-0 h-fit">
+
+hello
+    </div>
+  );
+}
